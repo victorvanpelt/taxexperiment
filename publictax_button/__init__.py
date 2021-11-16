027d9f46d4518b7bff2a946b290a0531903f5a62
Victor van Pelt <victorvanpelt@protonmail.com>
2021-11-16 12:23:44 +0100

    Update __init__.py

--- publictax_button/__init__.py
+++ publictax_button/__init__.py
@@ -15,7 +15,7 @@ class Constants(BaseConstants):
     name_in_url = 'publictax_btn'
     players_per_group = None
     num_rounds = 1
-    completion_code = 'TAX_uvt_032020'
+    completion_code = 'TAX_whu_112021'
     AgreeChoices = [
         [1, 'Strongly disagree'],
         [2, 'Disagree'],
